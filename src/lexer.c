df8d2fbef67048c2b63acdbd5b1bef87c8505299
Francisco Souza <francissouza1991@gmail.com>
2026-01-03 17:19:46 -0300

    feat: add comment support

--- src/lexer.c
+++ src/lexer.c
@@ -1,59 +1,79 @@
 #include <ctype.h>
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "lexer.h"
 
-static void skip_whitespace(Lexer* l) {
-    while(l->input[l->pos] && isspace(l->input[l->pos])) {
-        l->pos++;
-    }
-}
-
-static void consume_atom(Lexer* l) {
-    while (l->input[l->pos] != '\0' && !isspace(l->input[l->pos]) && 
-           l->input[l->pos] != '(' &&  l->input[l->pos] != ')') {
-        l->pos++;
-    }
-}
-
-static void consume_string(Lexer *l) {
-    l->pos++;
-
-    while (l->input[l->pos] != '\0' && l->input[l->pos] != '"') {
-        if (l->input[l->pos] == '\\' && l->input[l->pos + 1] == '"') {
-            l->pos += 2;
-        } else {
-            l->pos++;
-        }
-    }
-
-    if (l->input[l->pos] == '"') {
-        l->pos++;
-    }
+static size_t pos(Lexer* l) {
+    return l->pos;
 }
 
 static char peek(Lexer* l) {
-    skip_whitespace(l);
     return l->input[l->pos];
 }
 
 static char advance(Lexer* l) {
-    skip_whitespace(l);
+    if (l->input[l->pos] == '\0') {
+        return '\0';
+    }
+
     return l->input[l->pos++];
 }
 
-static char* build_char_token(char c) {
-    char* token = malloc(2 * sizeof(char));
-    token[0] = c;
-    token[1] = '\0';
-
-    return token;
+static size_t len(size_t start, size_t end) {
+    return end - start;
 }
 
-static char* build_lexer_token(Lexer* l, size_t start, size_t len) {
+static void skip_junk(Lexer* l) {
+    while (true) {
+        if (isspace(peek(l))) {
+            advance(l);
+        } 
+        else if (peek(l) == ';') {
+            while (peek(l) != '\0' && peek(l) != '\n') {
+                advance(l);
+            }
+        } else {
+            break;
+        }
+    }
+}
+
+static bool is_atom_delimiter(char c) {
+    return c != '\0' && !isspace(c) && c != '(' &&  c != ')' && c != ';';
+}
+
+static void consume_atom(Lexer* l) {
+    while (is_atom_delimiter(peek(l))) {
+        advance(l);
+    }
+}
+
+static void consume_string(Lexer *l) {    
+    while (peek(l) != '\0') {
+        char current = advance(l);
+
+        if (current == '\\' && peek(l) != '\0') {
+            advance(l);
+        } 
+
+        else if (current == '"') {
+            return;
+        }
+    }
+
+    printf("Error: string without matching quotes.");
+}
+
+static char* build_token(Lexer* l, size_t start, size_t len) {
     char* token = malloc(len + 1);
+
+    if (!token) {
+        return NULL;
+    }
 
     memcpy(token, &l->input[start], len);
     token[len] = '\0';
@@ -62,28 +82,26 @@ static char* build_lexer_token(Lexer* l, size_t start, size_t len) {
 }
 
 char* get_token(Lexer* l) {
-    char c = peek(l);
+    skip_junk(l);
 
-    if (c == '\0') {
+    size_t start = pos(l);
+    char target = peek(l);
+
+    if (target == '\0') {
         return NULL;
     }
 
-    if (c == '(' || c == ')') {
+    if (target == '(' || target == ')') {
         advance(l);
-        return build_char_token(c);
+        return build_token(l, start, 1);
     }
 
-    if(c == '"') {
-        size_t start = l->pos;
+    if(target == '"') {
+        advance(l);
         consume_string(l);
-        
-        size_t len = l->pos - start;
-        return build_lexer_token(l, start, len);
+        return build_token(l, start, len(start, pos(l)));
     }
 
-    size_t start = l->pos;
     consume_atom(l);
-
-    size_t len = l->pos - start;
-    return build_lexer_token(l, start, len);
+    return build_token(l, start, len(start, pos(l)));
 }
